d94f6494ce171f8a21e797b178dc74ff6d15ae3e
TEJAS-SHINDEE <tejasshindee2024@gmail.com>
2025-06-23 16:47:47 +0530

    fix: all backend links are set

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-// server.js
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 const app = express();
 
-// Middlewares
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
-// Routes
+app.get("/", (req, res) => {
+  res.send("Server is live");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-// DB & Server start
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
